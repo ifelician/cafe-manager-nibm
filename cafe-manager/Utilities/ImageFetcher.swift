ac641574c63bfa464ae420b13b079cf93c02a052
Felician Kudhoos <isharkfelician@gmail.com>
2021-05-14 05:36:12 +0530

    first commit

---
+++ cafe-manager/Utilities/ImageFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  ImageFetcher.swift
+//  cafe-manager
+//
+//  Created by Felician Ishark on 2021-05-14.
+//
+
+import Foundation
---
+++ cafe-manager/View/XIB/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  cafe-manager
+//
+//  Created by Felician Ishark on 2021-05-14.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
